45ddc1198f4e4b2541f0d739f7073619c928319d
Dnpypy <denlatypoff@yandex.ru>
2019-10-07 23:54:47 +0300

    edit app.rb condition

--- homework_lesson_22/app.rb
+++ homework_lesson_22/app.rb
@@ -17,10 +17,32 @@ get "/visit" do
 end
 
 post "/visit" do
+	
 	@login = params[:login]
-	@pass = params[:pass]
+	@mail = params[:mail]
 	@phone = params[:phone]
 	@datetime = params[:datetime]
+
+	@x = @login.class
+	@message = "login: #{@login}, pass: #{@mail}, phone: #{@phone}, datetime: #{@datetime}"
+
+	File.open("./public/users.txt", "a") do |file|
+		file.puts @message
+	end
+
+	if @login == ""
+		@user = "Введите пользователя!" 
+		erb :visit
+	elsif @mail == ""
+		@mail = "Введите почту!" 
+		erb :visit
+	elsif @phone == ""
+		@phone = "Введите телефон!" 
+		erb :visit
+	else
+		@welcome = "Вы успешно записались!"
+		erb :complete
+	end
 end
 
 get "/contacts" do
