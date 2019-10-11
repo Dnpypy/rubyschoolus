e19b4986b076b3c0253cea4cee180a7c90b1a0a9
Dnpypy <denlatypoff@yandex.ru>
2019-10-11 06:22:04 +0300

    create visit.erb lesson_23

--- lesson_23/app.rb
+++ lesson_23/app.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+﻿#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -6,3 +6,45 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get "/visit" do
+	erb :visit
+end
+
+post "/visit" do
+	
+	@barber   = params[:barber]
+	@login    = params[:login]
+	@mail     = params[:mail]
+	@phone    = params[:phone]
+	@datetime = params[:datetime]
+
+	if @login == ""
+		@check_user = "Введите пользователя!" 
+		erb :visit
+	
+	elsif @mail == "" 
+		@check_mail = "Введите почту!" 
+		erb :visit
+	
+	elsif @phone == ""
+		@check_phone = "Введите телефон!" 
+		erb :visit
+	
+	elsif @datetime == ""
+		@check_date = "Введиту дату прибытия!" 
+		erb :visit
+	
+	else
+		@message = "barber: #{@barber}, login: #{@login}, mail: #{@mail}, phone: #{@phone}, datetime: #{@datetime}"
+
+		File.open("./public/users.txt", "a") do |file|
+			file.puts @message
+		end
+
+		@welcome = "Вы успешно записались!"
+		erb :complete
+	end
+
+end
+
