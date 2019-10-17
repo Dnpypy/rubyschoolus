580c9da46a203765f217f106cefaaa7ecece3c10
Dnpypy <denlatypoff@yandex.ru>
2019-10-17 09:40:31 +0300

    initial commit
    create lesson_24

---
+++ lesson_24/README.md
@@ -0,0 +1,16 @@
+sinatra-bootstrap-clean
+=======================
+
+this folder github rubyschool-us:
+git clone https://github.com/rubyschool-us/sinatra-bootstrap-clean.git
+
+this folder cleaned github bootstrap-ruby: 
+Cleaned version of https://github.com/bootstrap-ruby/sinatra-bootstrap
+
+    edit GEMFILE                     # gem 'sinatra-contrib' # To work reloader
+    
+    bundle install                   # To install sinatra bundle
+
+    bundle update                    # install gems --> bundle
+
+    start bundle exec ruby app.rb    # To run the sample cmd window
---
+++ lesson_24/app.rb
@@ -0,0 +1,38 @@
+﻿#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get "/about" do
+	erb :about
+end
+
+get "/something" do
+	erb :something
+end
+
+
+get "/contacts" do
+	erb :contacts
+end
+
+get "/visit" do
+	erb :visit
+end
+
+post "/visit" do
+	
+	@login    = params[:login]
+	@phone    = params[:phone]
+	@datetime = params[:datetime]
+	@barber   = params[:barber]
+
+	@color_choice = params[:color_choice]
+
+	erb "ok this is username: #{@color_choice}, #{@login}, #{@phone}, #{@datetime}, #{@barber}"
+end
+
