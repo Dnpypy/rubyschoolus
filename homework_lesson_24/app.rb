e57cb1b8f996123ce38418c16ffde9dfaf7a738a
Dnpypy <denlatypoff@yandex.ru>
2019-10-18 08:52:35 +0300

    add hundler get "about", "contacts", "something"

--- homework_lesson_24/app.rb
+++ homework_lesson_24/app.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+﻿#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -6,3 +6,16 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get "/contacts" do
+	erb :contacts
+end
+
+get "/about" do
+	erb "О нас"
+end
+
+get "/something" do
+	erb "<h1>Что-то</h1>"
+end
+
