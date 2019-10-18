01f65450a800347b7c38b5db6b2d651abd663d41
Dnpypy <denlatypoff@yandex.ru>
2019-10-18 10:02:15 +0300

    add condition app.rb

--- homework_lesson_24/app.rb
+++ homework_lesson_24/app.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "pony"
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -19,3 +20,33 @@ get "/something" do
 	erb "<h1>Что-то</h1>"
 end
 
+post "/contacts" do
+
+	@login   = params[:login]
+	@mail    = params[:mail]
+	@message = params[:message]
+
+
+	data_info = { :login   => "Введите логин",
+	              :mail    => "Введите почту @",
+	              :message => "Вы не ввели сообщение!" }
+
+	# data_info.each do |key, value|
+
+	# 	if params[key] == ""
+	# 		@error = data_info[key]
+	# 		return erb :contacts
+	# 	end
+	# end 
+
+	@error = data_info.select { |key, _| params[key] == ""}.values.join(", ")
+
+	if @error != ""
+		return erb :contacts
+	end
+
+	erb "ok this is username: #{@login}, #{@mail}, #{@message}"
+
+
+	# erb :contacts
+end
