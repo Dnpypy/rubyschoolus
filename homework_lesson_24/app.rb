34dcdbfd96d565c6c2711b23ae5467273a6e394d
Dnpypy <denlatypoff@yandex.ru>
2019-10-21 12:38:54 +0300

    edit homework_lesson24

--- homework_lesson_24/app.rb
+++ homework_lesson_24/app.rb
@@ -1,8 +1,18 @@
-﻿#encoding: utf-8
+﻿if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+
+#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-# require "pony"
+require "pony"
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -38,15 +48,25 @@ post "/contacts" do
 		return erb :contacts
 	end
 
-	# Pony.mail(:to => '*denlat1981@gmail.com*', :from => "#{@mail}", :subject => "art inquiry from #{@login}", :body => "#{@message}")
 
-	Pony.mail(to: "denlat1981@gmail.com", 
-          from:     params[:login],
-          reply_to: params[:email],
-          subject:  "Message for #{params[:message]}",
-          body:     params[:message])
-
-	erb "ok this is username: #{@login}, #{@mail}, #{@message}"
-
+Pony.mail({
+	       :to      => 'test.test5847@gmail.com',
+		   :from    => "#{@mail}",
+		   :via     => :smtp,
+		   :subject => "Новое сообщение от пользователя #{@login}",
+		   :body    => "#{@message}",
+		   
+		    :via_options => {
+		    
+		    :address              => 'smtp.gmail.com',
+		    :port                 => '587',
+		    :enable_starttls_auto => true,
+		    :user_name            => 'testing879456',
+		    :password             => 'ZM542352soJPD9Ayr',
+		    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+		    :domain               => "localhost" # the HELO domain provided by the client to the server
+		  }
+		})
+		  erb :contacts
 
 end
