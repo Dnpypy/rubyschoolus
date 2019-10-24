bb718669c9d6ab8f631b5531af6957c4e7e38262
Dnpypy <denlatypoff@yandex.ru>
2019-10-24 12:57:53 +0300

    add value db name price

--- lesson_25/app.rb
+++ lesson_25/app.rb
@@ -3,9 +3,11 @@ require 'sqlite3'
 # строка подключения базы данных
 db = SQLite3::Database.new 'test.sqlite'
 
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+
 db.execute "SELECT * FROM Cars" do |car|
   puts car
-  puts "====="
+  puts "======"
 end
 
 db.close
