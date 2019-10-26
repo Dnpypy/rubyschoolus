5deae0f71cfc5af5f13d5b3a8603d98478ee42c1
Den <YasmDen@localhost.localdomain>
2019-10-26 22:08:38 +0300

    Keep barber options:

--- lesson_26/app.rb
+++ lesson_26/app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
 get "/about" do
@@ -25,7 +25,7 @@ get "/visit" do
 end
 
 post "/visit" do
-	
+
 	@login        = params[:login]
 	@mail         = params[:mail]
 	@phone        = params[:phone]
@@ -35,15 +35,15 @@ post "/visit" do
 	@color_choice = params[:color_choice]
 
 	# хэш
-	hh = {    :login => "Введите имя", 
-	           :mail => "Введите почту @", 
+	hh = {    :login => "Введите имя",
+	           :mail => "Введите почту @",
 		      :phone => "Введите телефон",
 		   :datetime => "Введите дату и время"
 		 }
 
 	# # для каждой пары ключ значение
  #    hh.each do |key, value|
-    	
+
  #    	# key == :login , key == :phone и т.д.
  #    	# если параметр пуст
  #    	if params[key] == ""
@@ -55,7 +55,7 @@ post "/visit" do
  #    		return erb :visit
 	# 	end
 	# end
-		
+
 	# выводит если не ввели n-e количество параметров
 	# Для получения отдельно массива ключей или значений существуют методы .keys и .values.
 	# .join соединяем их через запятую ", "
@@ -67,5 +67,6 @@ post "/visit" do
 
 	erb "ok this is username: #{@color_choice}, #{@login}, #{@phone}, #{@datetime}, #{@barber}"
 
-end
 
+
+end
