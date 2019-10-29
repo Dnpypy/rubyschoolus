bfc6c07e8aa9e545f20674846fb92111415a3803
Dnpypy <denlatypoff@yandex.ru>
2019-10-29 15:37:46 +0300

    add transfer @barbers --> before do

--- 26_homework_lesson/app.rb
+++ 26_homework_lesson/app.rb
@@ -6,13 +6,24 @@ require 'sinatra/reloader'
 # подгружаем библиотеку sqlite3
 require 'sqlite3'
 
-# configure - при запуске программы
-# before - при каждом обращении к программе
-
-# Синтаксис before (в sinatra) - исполняет код
-#  перед запросом - будет доступно во всех представлениях
-before do
+# Method validation data Options table in database
+def is_barber_exists? base, param
+	# 1 часть выражения base.execute()
+	# 2 часть выражения length > 0
+  base.execute('SELECT * FROM Barbers WHERE barber=?', [param]).length > 0
 end
+
+# Method add data to table in database
+# seed наполнить
+def seed_db base, options
+  options.each do |option|
+		# ! отрицание, если парихмахер не существует выполнить запрос
+    if !is_barber_exists? @db, option
+      base.execute 'INSERT INTO Barbers (barber) VALUES (?)', [option]
+    end
+  end
+end
+
 
 # создаем базу в файле barbershop.db
 def get_db
@@ -21,20 +32,16 @@ def get_db
 		return @db
 end
 
-# Method validation data Options table in database
-def is_option_exists? base, param
-  base.execute('SELECT * FROM Barbers WHERE barber=?', [param]).length > 0
-end
+# configure - при запуске программы
+# before - при каждом обращении к программе
 
-# Method add data to table in database
-def seed_db base, options
-  options.each do |option|
-    if !is_option_exists? @db, option
-      base.execute 'INSERT INTO Barbers (barber) VALUES (?)', [option]
-    end
-  end
+# Синтаксис before (в sinatra) - исполняет код
+#  перед запросом - будет доступно во всех представлениях
+# @barbers будет во всех представлениях
+before do
+	db = get_db
+	@barbers = @db.execute 'SELECT * FROM Barbers'
 end
-
 
 # инициализация приложения(базы данных) при старте
 # если файла barbershop.db нет, он будет создан в тек. каталоге приложения
@@ -59,6 +66,7 @@ configure do
 									"Id" INTEGER PRIMARY KEY AUTOINCREMENT,
 									"barber" TEXT
 								)'
+		# наполнение
 		seed_db @db, ['Foo', 'Faa', 'Moo', 'Zoo', 'Faz', 'Maz', 'Kraz']
 
 	   @db.close
@@ -82,9 +90,9 @@ end
 
 get "/visit" do
 	# Write to array data from database table Options
-	 get_db
-	 @barbers = @db.execute 'SELECT * FROM Barbers'
-	 @db.close
+	 # get_db
+	 # @barbers = @db.execute 'SELECT * FROM Barbers'
+	 # @db.close
 
 	 @title = "Форма заявки для Sinatra (Ruby)"
 
@@ -143,7 +151,8 @@ post "/visit" do
 end
 
 get '/admin/show' do
-	get_db
+	# получаем объект базы данных
+	@db = get_db
 	# Выбирает (SELECT) ВСЕ записи из (FROM) таблицы tbl_name
 	# и сортирует их (ORDER BY) по полю id в ОБРАТНОМ порядке.
 	@results = @db.execute 'SELECT * FROM Users ORDER BY id DESC'
